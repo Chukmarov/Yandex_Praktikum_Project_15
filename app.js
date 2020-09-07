5335d5d0356e5868fa959dbeb408b9fb243a89fd
Chukmarov <61390466+Chukmarov@users.noreply.github.com>
2020-09-07 22:26:06 +0500

    добавил класс ошибок

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).send({message: 'На сервере произошла ошибка'});
+  res.status(err.statusCode).send({ message: err.message });
 });
 
 app.listen(PORT, () => {});
