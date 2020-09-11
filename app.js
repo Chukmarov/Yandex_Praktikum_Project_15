baddb0ef2b8beb172a4974e8b994a396b121f0fb
Chukmarov <61390466+Chukmarov@users.noreply.github.com>
2020-09-11 22:36:37 +0500

    добавил логер запросов и ошибок к проекту, вторая попытка

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(helmet());
 
 app.use(requestLogger);
 
-app.post('/signin',celebrate({
+app.post('/signin',vcelebrate({
   [Segments.BODY]: Joi.object({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
