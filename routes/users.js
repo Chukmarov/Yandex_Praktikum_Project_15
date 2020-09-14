4da6f4f318134fae313799668e7eefc503a1b555
Chukmarov <61390466+Chukmarov@users.noreply.github.com>
2020-09-14 20:56:21 +0500

    Подправил ошибки линтера

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const { getUsers, getUserById } = require('../controllers/users');
 const {
   celebrate, Joi, Segments,
 } = require('celebrate');
+const { getUsers, getUserById } = require('../controllers/users');
 
 router.get('/', getUsers);
 
-router.get('/:userid',celebrate({
+router.get('/:userid', celebrate({
   [Segments.PARAMS]: Joi.object({
     userid: Joi.string().hex(),
   }),
